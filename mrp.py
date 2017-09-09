ced6919780e68d26dd787bf6f97d32fe450a0119
Nguyen Quoc Viet <vietnq69@gmail.com>
2017-09-09 10:46:45 +0700

    change to integer programming

--- mrp.py
+++ mrp.py
@@ -1,12 +1,14 @@
-from scipy.optimize import linprog
+from pulp import *
 import data
+import data2
+import data3
 
-bom = data.bom
-materials = data.materials
+bom = data2.bom
+materials = data2.materials
 
 # needed number of each part for the level that part appears
 # for example ['A_1','A_2','B_1',...] is number of A in level 1,2 and number of B in level 1
-variables = []
+lp_vars = []
 
 # variables for Simplex problem
 # check: https://docs.scipy.org/doc/scipy-0.18.1/reference/generated/scipy.optimize.linprog.html
@@ -22,18 +24,21 @@ def get_variables():
     for part, num in materials.iteritems():
         for level, bill in bom.iteritems():
             if part in bill[0]:
-                variables.append(part + '_' + level)
+                lp_var = LpVariable(name=part + '_' + level,
+                                    lowBound=0,
+                                    upBound=num,
+                                    cat=LpInteger if isinstance(bill[1], int) else LpContinuous)
+                lp_vars.append(lp_var)
                 objective.append(0)
     # objective will be finding maximize of produced products, a number called K
-    # it will be find minimum of -K
-    objective.append(-1)
+    objective.append(1)
 
 
 def build_ub():
     for part, num in materials.iteritems():
         row = []
-        for var in variables:
-            if part == var.split('_')[0]:
+        for var in lp_vars:
+            if part == var.name.split('_')[0]:
                 row.append(1)
             else:
                 row.append(0)
@@ -49,44 +54,48 @@ def build_eq():
         coef = bill[1]
         options = bill[0]
 
-        for var in variables:
-            if var.split('_')[0] in options and var.split('_')[1] == level:
+        for var in lp_vars:
+            if var.name.split('_')[0] in options and var.name.split('_')[1] == level:
                 row.append(1)
             else:
                 row.append(0)
 
-        row.append(-coef) # coef for K
+        row.append(-coef)  # coef for K
         A_eq.append(row)
         B_eq.append(0)
 
 
 def build_bounds():
-    for var in variables:
-        bounds.append((0, materials[var.split('_')[0]]))
-    bounds.append((0, None)) # bounds for K
+    for var in lp_vars:
+        bounds.append((0, materials[var.name.split('_')[0]]))
+    bounds.append((0, None))  # bounds for K
+
 
 def main():
-    print 'Starting the optimization ...'
+    prob = LpProblem("Dong bo san pham", LpMaximize)
     get_variables()
     build_eq()
     build_ub()
     build_bounds()
 
-    print 'Variables: %s' % (variables)
+    lp_vars.append(LpVariable('K', 0, cat=LpInteger))
+    num_of_vars = len(lp_vars)
 
-    res = linprog(objective, A_eq=A_eq, b_eq=B_eq, A_ub=A_ub, b_ub=B_ub, bounds=bounds, options={"disp": True})
+    prob += lpSum([objective[i] * lp_vars[i] for i in xrange(num_of_vars)])
 
-    optimized = dict(zip(variables, res.x))
+    for i in xrange(len(A_eq)):
+        prob += lpSum([A_eq[i][j] * lp_vars[j] for j in xrange(num_of_vars)]) == B_eq[i]
 
-    print '=============================='
-    print 'BEST NUMBER: %d' % (-res.fun)
-    print '=============================='
+    for i in xrange(len(A_ub)):
+        prob += lpSum([A_ub[i][j] * lp_vars[j] for j in xrange(num_of_vars)]) <= B_ub[i]
 
-    for level, bill in bom.iteritems():
-        options = bill[0]
-        suggested = [optimized[option + '_' + level] for option in options]
-        tmp = dict(zip(options, suggested))
-        print 'Level: %s    %s' % (level, tmp)
+    prob.solve()
+
+    # Solution
+    for v in prob.variables():
+        print v.name, "=", v.varValue
+
+    print "objective=", value(prob.objective)
 
 
 if __name__ == '__main__':
